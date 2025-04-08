64beadcc777f5280262eb49be1c9576eeb812f49
rachel-mp4 <rachel@moth11.net>
2025-04-07 22:21:22 -0400

    basic http endpoint stuff

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,21 +2,91 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"sync"
+	"net/http"
 
 	"github.com/rachel-mp4/lrc/lrcd/pkg/lrcd"
 )
 
+var (
+	channelToServer map[string]*lrcd.Server
+)
+
 func main() {
+	channelToServer = make( map[string]*lrcd.Server)
 	fmt.Println("hello world")
-	server, err := lrcd.NewServer(lrcd.WithWSPort(8080), lrcd.WithLogging(os.Stdout, true))
-	err = server.Start()
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	var wg sync.WaitGroup
-	wg.Add(1)
-	wg.Wait()
-	return
+	http.HandleFunc("GET /", homeHandler)
+
+	http.HandleFunc("GET /{channel}", serverStart)
+	http.ListenAndServe(":8080", nil)
 }
+
+func serverStart(w http.ResponseWriter, r *http.Request) {
+	name := r.PathValue("channel")
+	fmt.Fprintln(w,name)
+	_, ok := channelToServer[name]
+	if ok {
+		fmt.Fprint(w, "already created server")
+		return
+	}
+	channelToServer[name] = nil
+	fmt.Fprintln(w, "created server")
+	// if server != nil {
+	// 	fmt.Fprint(w, "server already started")
+	// 	return
+	// }
+	// var err error
+	// server, err = lrcd.NewServer(lrcd.WithWSPort(8080), lrcd.WithLogging(os.Stdout, true))
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	fmt.Fprintln(w, "failed to start")
+	// 	return
+	// }
+	// err = server.Start()
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	fmt.Fprintln(w, "failed to start")
+	// 	return
+	// }
+	// fmt.Fprintln(w, "started")
+}
+
+func serverStop(w http.ResponseWriter, r *http.Request) {
+	// if server == nil {
+	// 	fmt.Fprintln(w, "no server to stop")
+	// 	return
+	// }
+	// err := server.Stop()
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	fmt.Fprintln(w, "failed to stop")
+	// 	return
+	// }
+	// server = nil
+	// fmt.Fprintln(w, "stopped")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "welcome")
+	for k, _ := range channelToServer {
+		fmt.Fprintln(w, k)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server/go.mod
+++ server/go.mod
@@ -2,10 +2,13 @@ module xcvr-backend
 
 go 1.22.1
 
-require github.com/rachel-mp4/lrc/lrcd v0.0.0-20250407195722-5a8956062fbc
+require github.com/rachel-mp4/lrc/lrcd v0.0.0-20250408005617-2d344a3d04f7
 
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/rachel-mp4/lrc v0.0.0-20250407195722-5a8956062fbc // indirect
-	github.com/rachel-mp4/lrc/lrc v0.0.0-20250407195722-5a8956062fbc // indirect
+	github.com/rachel-mp4/lrc/lrc v0.0.0-20250408005617-2d344a3d04f7 // indirect
 )
+
+replace github.com/rachel-mp4/lrc/lrcd => ../../lrc/lrcd
+
+replace github.com/rachel-mp4/lrc/lrc => ../../lrc/lrc
