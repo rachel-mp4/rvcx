e18712138278c1aeb0a265566f8cc24daad3f4fd
rachel-mp4 <rachel@moth11.net>
2025-04-09 23:56:27 -0400

    update some things

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
+	"os"
+	"slices"
+	"sync"
+	"time"
 
 	"github.com/rachel-mp4/lrc/lrcd/pkg/lrcd"
 )
 
 var (
-	channelToServer map[string]*lrcd.Server
-	channels []channel
+	channelsMu   sync.Mutex
+	bandToServer map[string]*lrcd.Server
+	channels     []channel
 )
 
 func main() {
-	channelToServer = make(map[string]*lrcd.Server)
+	bandToServer = make(map[string]*lrcd.Server)
+	channels = make([]channel, 0)
+	createChannel(channel{Band: "general", Sign: "hiiiiiiiii"}, false)
+	createChannel(channel{Band: "sneep", Sign: "snirp"}, true)
 	fmt.Println("hello world")
-	http.HandleFunc("GET /xrpc/getChannels", getChannels)
-
+	http.HandleFunc("GET /xrpc/getChannels", withCORS(getChannels))
 	http.HandleFunc("POST /xrpc/initChannel", initChannel)
 	http.ListenAndServe(":8080", nil)
 }
@@ -33,6 +41,7 @@ func getChannels(w http.ResponseWriter, r *http.Request) {
 type channel struct {
 	Band string `json:"band"`
 	Sign string `json:"sign"`
+	Port int    `json:"port"`
 }
 
 func initChannel(w http.ResponseWriter, r *http.Request) {
@@ -42,16 +51,85 @@ func initChannel(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	channels = append(channels, c)
+	createChannel(c, true)
+
 	fmt.Printf("created a channel on band: %s and call sign: %s\n", c.Band, c.Sign)
 }
 
+func createChannel(c channel, withDelete bool) error {
+	port, err := getFreePort()
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	c.Port = port
 
+	options := []lrcd.Option{lrcd.WithWSPort(c.Port), 
+		lrcd.WithWSPath(c.Band),
+		lrcd.WithWelcome(c.Sign),
+		lrcd.WithLogging(os.Stdout, true),
+	}
+	ec := make(chan struct{})
+	
+	if withDelete {
+		options = append(options,lrcd.WithEmptyChannel(ec))
+		after := 10*time.Second
+		options = append(options, lrcd.WithEmptySignalAfter(after))
+	}
+	server, err := lrcd.NewServer(options...)
 
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	fmt.Println("created", c.Band)
 
+	err = server.Start()
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+	fmt.Println("started", c.Band)
 
+	channelsMu.Lock()
+	defer channelsMu.Unlock()
+	bandToServer[c.Band] = server
+	channels = append(channels, c)
+	if withDelete {
+		go func() {
+			<-ec
+			channelsMu.Lock()
+			idx := slices.Index(channels, c)
+			channels = slices.Delete(channels, idx, idx+1)
+			err = bandToServer[c.Band].Stop()
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			delete(bandToServer, c.Band)
+			channelsMu.Unlock()
+			fmt.Println("deleted", c.Band)
+		}()
+	}
+	return nil
+}
 
+func getFreePort() (int, error) {
+	nl, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return -1, err
+	}
+	defer nl.Close()
+	return (nl.Addr().(*net.TCPAddr)).Port, nil
+}
 
-
-
-
+func withCORS(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if r.Method == "OPTIONSONS" {
+			w.WriteHeader(http.StatusNoContent)
+		}
+		h(w, r)
+	}
+}
--- server/go.mod
+++ server/go.mod
@@ -2,11 +2,11 @@ module xcvr-backend
 
 go 1.22.1
 
-require github.com/rachel-mp4/lrc/lrcd v0.0.0-20250408005617-2d344a3d04f7
+require github.com/rachel-mp4/lrc/lrcd v0.0.0-20250410002721-ca6a18431212
 
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/rachel-mp4/lrc/lrc v0.0.0-20250408005617-2d344a3d04f7 // indirect
+	github.com/rachel-mp4/lrc/lrc v0.0.0-20250408013928-75dc71a6060f // indirect
 )
 
 replace github.com/rachel-mp4/lrc/lrcd => ../../lrc/lrcd
