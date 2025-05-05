09bd60f9d7f88530b1ae04171e2af41f32dd6558
rachel-mp4 <rachel@moth11.net>
2025-05-05 11:36:18 -0400

    add migrations for current lexicon

--- migrations/001_init.up.sql
+++ migrations/001_init.up.sql
@@ -1,3 +1,22 @@
+CREATE TABLE profiles (
+	did TEXT PRIMARY KEY,
+	display_name TEXT,
+	default_nick TEXT,
+	status TEXT,
+	avatar_cid TEXT,
+	avatar_mime TEXT,
+	color INTEGER CHECK (color BETWEEN 0 and 16777215),
+	uri TEXT NOT NULL UNIQUE,
+	cid TEXT NOT NULL,
+	indexed_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE did_handle (
+	handle TEXT PRIMARY KEY,
+	did TEXT NOT NULL UNIQUE,
+	indexed_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
 CREATE TABLE channels (
 	uri TEXT PRIMARY KEY,
 	cid TEXT NOT NULL,
@@ -8,3 +27,29 @@ CREATE TABLE channels (
 	created_at TIMESTAMPTZ NOT NULL,
 	indexed_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
+
+CREATE TABLE signets (
+	uri TEXT PRIMARY KEY,
+	did TEXT NOT NULL,
+	channel_uri TEXT NOT NULL,
+	FOREIGN KEY (channel_uri) REFERENCES channels(uri) ON DELETE CASCADE,
+	message_id INTEGER CHECK (message_id BETWEEN 0 AND 4294967295),
+	cid TEXT NOT NULL,
+	indexed_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX ON signets (channel_uri, message_id DESC);
+
+CREATE TABLE messages (
+	uri TEXT PRIMARY KEY,
+	did TEXT NOT NULL,
+	signet_uri TEXT NOT NULL,
+	FOREIGN KEY (signet_uri) REFERENCES signets(uri) ON DELETE CASCADE,
+	body TEXT,
+	nick TEXT NOT NULL DEFAULT 'wanderer',
+	color INTEGER CHECK (color BETWEEN 0 AND 16777215),
+	cid TEXT NOT NULL,
+	indexed_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX ON messages (signet_uri);
--- migrations/002_init.down.sql
+++ migrations/002_init.down.sql
@@ -1,1 +1,9 @@
+DROP INDEX IF EXISTS messages_signet_uri_idx;
+DROP TABLE messages;
+
+DROP INDEX IF EXISTS signets_channel_uri_message_id_idx;
+DROP TABLE signets;
+
 DROP TABLE channels;
+DROP TABLE did_handle;
+DROP TABLE profiles;
