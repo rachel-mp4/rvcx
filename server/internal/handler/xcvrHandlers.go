212b6266b615a93a7c7a3ae3dc340db079ddaf34
rachel-mp4 <rachel@moth11.net>
2025-06-25 15:09:12 -0400

    add validation for postProfile

--- server/internal/handler/xcvrHandlers.go
+++ server/internal/handler/xcvrHandlers.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"encoding/json"
 	"errors"
+	"github.com/rivo/uniseg"
 	"net/http"
+	"unicode/utf16"
 	"xcvr-backend/internal/db"
 	"xcvr-backend/internal/types"
 )
@@ -24,22 +26,52 @@ func (h *Handler) postProfile(w http.ResponseWriter, r *http.Request) {
 	var pu db.ProfileUpdate
 	pu.DID = did
 	if p.DisplayName != nil {
+		if uniseg.GraphemeClusterCount(*p.DisplayName) > 64 {
+			h.badRequest(w, errors.New("too many graphemes"))
+			return
+		}
+		runes := []rune(*p.DisplayName)
+		us := utf16.Encode(runes)
+		if len(us) > 640 {
+			h.badRequest(w, errors.New("too many utf16 code points"))
+			return
+		}
 		pu.Name = p.DisplayName
 		pu.UpdateName = true
 	}
 	if p.DefaultNick != nil {
+		runes := []rune(*p.DefaultNick)
+		us := utf16.Encode(runes)
+		if len(us) > 16 {
+			h.badRequest(w, errors.New("too many utf16 code points"))
+			return
+		}
 		pu.Nick = p.DefaultNick
 		pu.UpdateNick = true
 	}
 	if p.Status != nil {
+		if uniseg.GraphemeClusterCount(*p.DisplayName) > 640 {
+			h.badRequest(w, errors.New("too many graphemes"))
+			return
+		}
+		runes := []rune(*p.DisplayName)
+		us := utf16.Encode(runes)
+		if len(us) > 6400 {
+			h.badRequest(w, errors.New("too many utf16 code points"))
+			return
+		}
 		pu.Status = p.Status
 		pu.UpdateStatus = true
 	}
 	if p.Avatar != nil {
+		// TODO think about how to do avatars!
 		pu.Avatar = p.Avatar
 		pu.UpdateAvatar = true
 	}
 	if p.Color != nil {
+		if *p.Color > 16777215 || *p.Color < 0 {
+			h.badRequest(w, errors.New("color out of bounds"))
+		}
 		pu.Color = p.Color
 		pu.UpdateColor = true
 	}
