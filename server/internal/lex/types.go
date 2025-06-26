b9b2bd92a20297b011a47f4fc43e6c69e022c9f5
rachel-mp4 <rachel@moth11.net>
2025-06-26 18:14:18 -0400

    fix syntax and remove dummy

--- server/internal/lex/types.go
+++ server/internal/lex/types.go
@@ -2,22 +2,13 @@ package lex
 
 import (
 	"github.com/bluesky-social/indigo/lex/util"
-	"io"
 )
 
-func init() {
-	util.RegisterType("org.xcvr.actor.profile", &ProfileRecord{})
-}
-
 type ProfileRecord struct {
-	LexiconTypeID string        `json: "$type,const=org.xcvr.actor.profile" cborgen:"$type,const=org.xcvr.actor.profile"`
+	LexiconTypeID string        `json:"$type,const=org.xcvr.actor.profile" cborgen:"$type,const=org.xcvr.actor.profile"`
 	DisplayName   *string       `json:"displayName,omitempty" cborgen:"displayName,omitempty"`
 	DefaultNick   *string       `json:"defaultNick,omitempty" cborgen:"defaultNick,omitempty"`
 	Status        *string       `json:"status,omitempty" cborgen:"status,omitempty"`
 	Avatar        *util.LexBlob `json:"avatar,omitempty" cborgen:"avatar,omitempty"`
 	Color         *uint64       `json:"color,omitempty" cborgen:"color,omitempty"`
-}
-
-func (p *ProfileRecord) MarshalCBOR(w io.Writer) error {
-	panic("run ./gen")
 }
