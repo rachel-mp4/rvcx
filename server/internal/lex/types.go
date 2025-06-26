1eb7d51f1a304b47af0e931c635caeddd2208bbe
rachel-mp4 <rachel@moth11.net>
2025-06-26 17:59:32 -0400

    add dummy method

--- server/internal/lex/types.go
+++ server/internal/lex/types.go
@@ -1,9 +1,23 @@
 package lex
 
+import (
+	"github.com/bluesky-social/indigo/lex/util"
+	"io"
+)
+
+func init() {
+	util.RegisterType("org.xcvr.actor.profile", &ProfileRecord{})
+}
+
 type ProfileRecord struct {
-	DisplayName *string       `json:"displayName,omitempty" cborgen:"displayName,omitempty"`
-	DefaultNick *string       `json:"defaultNick,omitempty" cborgen:"defaultNick,omitempty"`
-	Status      *string       `json:"status,omitempty" cborgen:"status,omitempty"`
-//	Avatar      *util.LexBlob `json:"avatar,omitempty" cborgen:"avatar,omitempty"`
-	Color       *uint64       `json:"color,omitempty" cborgen:"color,omitempty"`
+	LexiconTypeID string        `json: "$type,const=org.xcvr.actor.profile" cborgen:"$type,const=org.xcvr.actor.profile"`
+	DisplayName   *string       `json:"displayName,omitempty" cborgen:"displayName,omitempty"`
+	DefaultNick   *string       `json:"defaultNick,omitempty" cborgen:"defaultNick,omitempty"`
+	Status        *string       `json:"status,omitempty" cborgen:"status,omitempty"`
+	Avatar        *util.LexBlob `json:"avatar,omitempty" cborgen:"avatar,omitempty"`
+	Color         *uint64       `json:"color,omitempty" cborgen:"color,omitempty"`
+}
+
+func (p *ProfileRecord) MarshalCBOR(w io.Writer) error {
+	panic("run ./gen")
 }
